aff6e4f69fad418ec8a6cd4f89181588635caa4a
Silloky (Elias K.) <ek.mindstorms@outlook.com>
2025-06-23 12:49:37 +0200

    ref: empty repository

--- www/gala-scanner/redirection.js
+++
@@ -1,9 +0,0 @@
-const options = document.getElementsByClassName('option');
-
-for (let i = 0; i < options.length; i++) {
-    const option = options[i];
-    const link = option.getAttribute('data-link');
-    option.addEventListener('click', () => {
-        window.location = link;
-    })
-}
